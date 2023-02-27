c58a454bcf24286cf5c2271928f09eae384d40c1
Talfaza <42444571+Talfaza@users.noreply.github.com>
2023-02-27 01:35:34 +0100

    Add files via upload

---
+++ .config/nvim/lua/core/plugin_config/nvim-tree.lua
@@ -0,0 +1,25 @@
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require("nvim-tree").setup()
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    width = 30,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
+
+vim.keymap.set('n', '<c-n>', ':NvimTreeFindFileToggle<CR>')
